116fb3d6fabbd55c9a037cab6944c181e503a596
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-12 14:30:36 -0300

    feat: error screen

---
+++ components/errorMessage/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Head from "next/head";
+import { Div } from "../../styles/customDiv";
+
+export default function ErrorMessage({ error }) {
+  return (
+    <>
+      <Head>
+        <title>Error | Pokédex</title>
+      </Head>
+
+      <Div bg="#fff" mt="2.4rem" p="3.2rem" borderRadius="0.8rem">
+        <Div maxWidth="300px">
+          <Div
+            fontSize="2.4rem"
+            textAlign="center"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Can't find "{error.query}"
+          </Div>
+
+          <Div mt="3.2rem" mx="auto">
+            <Image
+              alt="pikachu sad"
+              width={300}
+              height={224}
+              src="/pikachu-sad.gif"
+            />
+          </Div>
+        </Div>
+      </Div>
+    </>
+  );
+}

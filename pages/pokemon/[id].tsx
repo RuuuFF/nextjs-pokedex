06e56c8d8cc54fa4e943a642277c413697c610d3
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-12 14:31:10 -0300

    feat: error screen

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,6 +1,6 @@
 import { getPokemon } from "../../utils/pokedex";
-import { Div } from "../../styles/customDiv";
 import Layout from "../../components/layout";
+import ErrorMessage from "../../components/errorMessage";
 import PageCard from "../../components/pageCard/pageCard";
 
 export async function getServerSideProps(context) {
@@ -13,22 +13,9 @@ export async function getServerSideProps(context) {
 }
 
 export default function PokemonPage({ pokemon, error }) {
-  console.log(pokemon, error);
   return (
     <Layout>
-      {error ? (
-        <Div
-          mt="3.2rem"
-          padding="4rem"
-          borderRadius="0.8rem"
-          bg="#fff"
-          fontSize="3.2rem"
-        >
-          Can't find "{error.query}"
-        </Div>
-      ) : (
-        <PageCard {...pokemon} />
-      )}
+      {error ? <ErrorMessage error={error} /> : <PageCard {...pokemon} />}
     </Layout>
   );
 }

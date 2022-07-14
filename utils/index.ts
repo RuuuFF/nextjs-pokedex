a6b71f97d89b5a38ac08afb5fae1f98ff2d8fb8c
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-14 11:54:03 -0300

    fix: pokemon name formatter

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,8 @@
 export function formatText(text: string) {
-  return text[0].toUpperCase() + text.slice(1);
+  return text
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 export function formatId(id: string | number) {

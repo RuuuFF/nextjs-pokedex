29ae266afe733bf4d7b73b8a0c2d37621d626b17
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-15 16:56:04 -0300

    feat: pokemon status and abilities

--- components/pokemonPage/index.tsx
+++ components/pokemonPage/index.tsx
@@ -1,20 +1,27 @@
 import Head from "next/head";
 import { PokemonProps } from "../../utils/pokedex";
-import { formatId, formatText, getPokemonImageSource } from "../../utils";
+import {
+  formatId,
+  formatText,
+  formatNumber,
+  getPokemonImageSource,
+} from "../../utils";
 
-import { Div } from "../../styles/customDiv";
 import Picture from "../picture";
 import Evolutions from "../evolutions";
+import { Div } from "../../styles/customDiv";
 import { Container } from "./style";
 
 export default function PokemonPage({ pokemon }: { pokemon: PokemonProps }) {
-  const { id, name, types, stats, weight, abilities, evolution_chain } =
+  const { id, name, types, stats, weight, height, abilities, evolution_chain } =
     pokemon;
   const formattedName = formatText(name);
   const formattedId = formatId(id);
-  const pageTitle = `${formattedName} | Next.js Pokédex`;
+  const formattedWeight = formatNumber(weight);
+  const formattedHeight = formatNumber(height);
   const mainType = types[0];
   const image = getPokemonImageSource(formattedId);
+  const pageTitle = `${formattedName} | Next.js Pokédex`;
 
   return (
     <Container>
@@ -29,26 +36,63 @@ export default function PokemonPage({ pokemon }: { pokemon: PokemonProps }) {
         </h1>
       </Div>
 
-      <div className="image-stat-container">
-        <div className="poke-image-container">
-          <Picture className="poke-image" src={image} alt={name} />
-        </div>
-
-        <div className="stat-container">
-          {stats?.map((stat, index) => {
-            // const statPercentage = (stat.base_stat * 100) / 220;
-            const formattedStatName = formatText(stat.name);
-
-            return (
-              <Div className="stat" width="48%" key={index}>
-                <div className="stat-name">
-                  {formattedStatName} {stat.base_stat}
-                </div>
+      <Div display="grid" gridtc="1fr 1fr, 1fr" gap="1.6rem">
+        <div className="column one">
+          <Div width="100%" maxWidth="36rem" bg="#aadfb2" borderRadius="0.8rem">
+            <Div width="100%" position="relative" pb="100%">
+              <Div position="absolute" inset="0" p="2.4rem">
+                <Picture className="poke-image" src={image} alt={name} />
               </Div>
-            );
-          })}
+            </Div>
+            {/* types */}
+          </Div>
         </div>
-      </div>
+
+        <div className="column two">
+          <Div className="stats" mb="1.6rem">
+            <div>
+              <div className="stat-name">Height</div>
+              <div className="stat-value">{formattedHeight}m</div>
+            </div>
+            <div>
+              <div className="stat-name">Weight</div>
+              <div className="stat-value">{formattedWeight}kg</div>
+            </div>
+
+            <Div gridc="1 / span 2">
+              <div className="stat-name">Abilities</div>
+              <div className="stat-value">
+                {abilities.map((abilitie) => formatText(abilitie)).join(", ")}
+              </div>
+            </Div>
+          </Div>
+
+          <div className="stats">
+            {stats?.map((stat, index) => {
+              const statPercentage = (stat.base_stat * 100) / 220;
+              const formattedStatName = formatText(stat.name);
+
+              return (
+                <div key={index}>
+                  <div
+                    className={`stat-name ${
+                      stat.name.length <= 2 ? "uppercase" : ""
+                    }`}
+                  >
+                    {formattedStatName}
+                  </div>
+                  <div className="stat-bar">
+                    <Div
+                      className={`stat-percentage ${mainType}`}
+                      width={`${statPercentage}%`}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Div>
 
       <Evolutions evolutions={evolution_chain} type={mainType} />
     </Container>
--- components/pokemonPage/style.ts
+++ components/pokemonPage/style.ts
@@ -1,3 +1,4 @@
+import { pokemonColorTypes } from "./../../styles/pokemonColors";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -7,25 +8,71 @@ export const Container = styled.div`
   border-radius: 0.8rem;
 
   .poke-name {
+    margin: 0.8rem 0 2.4rem;
     text-align: center;
-    font-size: 4rem;
+    font-size: 3.2rem;
     font-weight: 500;
-  }
 
-  .poke-id {
-    color: #999;
-  }
-
-  .image-stat-container {
-    display: flex;
-
-    & > * {
-      width: 50%;
+    .poke-id {
+      color: #999;
     }
   }
 
+  .column {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 2.4rem;
+  }
+
   .poke-image {
-    width: 32rem;
-    height: 32rem;
+    width: 100%;
+    height: 100%;
+  }
+
+  .stats {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1.6rem;
+    padding: 1.6rem;
+    width: 100%;
+    max-width: 40rem;
+    background-color: #aadfb2;
+    border-radius: 0.8rem;
+
+    .stat-name {
+      text-transform: capitalize;
+      font-size: 1.4rem;
+      font-weight: 600;
+
+      &.uppercase {
+        text-transform: uppercase;
+      }
+    }
+
+    .stat-value {
+      letter-spacing: 1px;
+      font-size: 2.2rem;
+      font-weight: 400;
+      margin-top: 0.4rem;
+    }
+
+    .stat-bar {
+      height: 1.8rem;
+      background-color: #fff;
+      margin-top: 0.8rem;
+      border-radius: 0.8rem;
+      overflow: hidden;
+    }
+
+    .stat-percentage {
+      height: inherit;
+      border-radius: inherit;
+      border: 1px solid #00000033;
+      ${pokemonColorTypes}
+      transition: width 0.6s ease;
+    }
   }
 `;

890a79444d0adf58c205509c6644f6b2c105e0ec
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-15 16:59:55 -0300

    format numbers function

--- utils/index.ts
+++ utils/index.ts
@@ -12,3 +12,15 @@ export function formatId(id: string | number) {
 export function getPokemonImageSource(id: string) {
   return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
 }
+
+export function formatNumber(number: number) {
+  const string = number.toString();
+  const length = string.length;
+
+  if (length <= 1) return "0." + number;
+
+  const a = string.substring(0, length - 1);
+  const b = string[length - 1];
+
+  return a + "." + b;
+}

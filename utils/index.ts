f77e864eba41818a0304034fc0cb5a383bce2dfe
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-18 13:54:32 -0300

    style: reorder functions

--- utils/index.ts
+++ utils/index.ts
@@ -9,10 +9,6 @@ export function formatId(id: string | number) {
   return id.toString().padStart(3, "0");
 }
 
-export function getPokemonImageSource(id: string) {
-  return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
-}
-
 export function formatNumber(number: number) {
   const string = number.toString();
   const length = string.length;
@@ -23,4 +19,8 @@ export function formatNumber(number: number) {
   const b = string[length - 1];
 
   return a + "." + b;
+}
+
+export function getPokemonImageSource(id: string) {
+  return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
 }

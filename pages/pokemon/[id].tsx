47b170a1c59ce079a6d4aa157eb26478902ce503
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-19 18:28:31 -0300

    revert: component PokemonPage name

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,7 +1,7 @@
 import { getPokemon } from "../../utils/pokedex";
 import Layout from "../../components/Layout";
 import ErrorCard from "../../components/ErrorCard";
-import PokePage from "../../components/PokePage";
+import PokemonPage from "../../components/PokemonPage";
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
@@ -15,7 +15,7 @@ export async function getServerSideProps(context) {
 export default function Pokemon({ pokemon, error }) {
   return (
     <Layout>
-      {!error ? <PokePage pokemon={pokemon} /> : <ErrorCard error={error} />}
+      {!error ? <PokemonPage pokemon={pokemon} /> : <ErrorCard error={error} />}
     </Layout>
   );
 }
